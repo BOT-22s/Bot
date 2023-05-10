9129194a466e74ceb938874cde05da0224566c7c
BOT-22s <127882997+BOT-22s@users.noreply.github.com>
2023-05-10 16:36:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import os
 import discord
-from discord_slash import SlashCommand
+import datetime
 from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
-slash = SlashCommand(bot, sync_commands=True) # Declares slash commands through the bot.
-
 
 @bot.event
 async def on_ready():
+    await bot.change_presence(activity=discord.Game(name="!help for commands"))
     print(f"Logged in as {bot.user}")
+
+@bot.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(f'Hi {member.name}, welcome to our Discord server!')
+
+@bot.command()
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, member : discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f'Banned {member.mention}')
 
 @bot.command()
 async def ping(ctx):
@@ -21,12 +31,29 @@ async def ping(ctx):
 async def hello(ctx):
     await ctx.send("Choo choo! 🚅")
 
-@slash.slash(name="ping")
-async def _ping(ctx):  # Defines a new "context" (ctx) command called "ping."
-    await ctx.send(content="pong")
+@bot.command()
+async def info(ctx):
+    embed = discord.Embed(title=f"{ctx.guild.name}", description="Lorem Ipsum asdasd", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
+    embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
+    embed.add_field(name="Server Owner", value=f"{ctx.guild.owner}")
+    embed.add_field(name="Server Region", value=f"{ctx.guild.region}")
+    embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
+    embed.set_thumbnail(url=f"{ctx.guild.icon_url}")
+    await ctx.send(embed=embed)
 
-@slash.slash(name="hello")
-async def _hello(ctx):  # Defines a new "context" (ctx) command called "hello."
-    await ctx.send(content="Choo choo! 🚅")
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Invalid command used.")
+
+@bot.command()
+async def userinfo(ctx, member: discord.Member):
+    embed = discord.Embed(title=f"{member}", description=f"Here is the info we retrieved about the user {member.mention}", color=discord.Color.dark_blue())
+    embed.add_field(name="ID", value=f"{member.id}", inline=True)
+    embed.add_field(name="Nickname", value=f"{member.nick}", inline=True)
+    embed.add_field(name="Created at", value=f"{member.created_at.strftime('%a, %#d %B %Y, %I:%M %p UTC')}", inline=False)
+    embed.add_field(name="Joined at", value=f"{member.joined_at.strftime('%a, %#d %B %Y, %I:%M %p UTC')}", inline=False)
+    embed.set_thumbnail(url=f"{member.avatar_url}")
+    await ctx.send(embed=embed)
 
 bot.run(os.environ["DISCORD_TOKEN"])
