78d5347de838732ef0dd763df8fc064d6612e5ec
BOT-22s <127882997+BOT-22s@users.noreply.github.com>
2023-05-10 16:30:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-# This example requires the 'message_content' privileged intents
-
 import os
 import discord
+from discord_slash import SlashCommand
 from discord.ext import commands
-
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
+slash = SlashCommand(bot, sync_commands=True) # Declares slash commands through the bot.
 
 
 @bot.event
@@ -22,5 +21,12 @@ async def ping(ctx):
 async def hello(ctx):
     await ctx.send("Choo choo! 🚅")
 
+@slash.slash(name="ping")
+async def _ping(ctx):  # Defines a new "context" (ctx) command called "ping."
+    await ctx.send(content="pong")
+
+@slash.slash(name="hello")
+async def _hello(ctx):  # Defines a new "context" (ctx) command called "hello."
+    await ctx.send(content="Choo choo! 🚅")
 
 bot.run(os.environ["DISCORD_TOKEN"])
