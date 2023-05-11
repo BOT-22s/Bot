83a9b932961a413575dd6c84e1e4ff39b8a52600
BOT-22s <127882997+BOT-22s@users.noreply.github.com>
2023-05-10 17:55:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime
 from discord.ext import commands
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='?', intents=intents)
 
 @bot.event
 async def on_ready():
